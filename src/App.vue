a0485cef33d3e7ff05732c009a555ae098e19da9
iamsarthakm <maheshwarisarthak1998@gmail.com>
2022-05-27 22:59:58 +0530

    Vuejs store module practice app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <AddTodo />
+      <FilterTodos />
+      <TodosList />
+    </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodosList from './components/TodosList.vue'
+import AddTodo from "./components/AddTodo.vue"
+import FilterTodos from './components/FilterTodos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddTodo, TodosList, FilterTodos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: "Franklin Gothoc Medium","Arial Narrow",Arial,sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
